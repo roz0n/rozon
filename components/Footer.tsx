0c916323c3c104e88ba23891fc2f189944c6411d
Arnold Rozon <arnold@rozon.org>
2021-06-18 00:33:53 -0500

    Spotify footer widget

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,27 @@
 import styles from "../styles/Footer.module.css";
+import Image from "next/image";
+import SpotifyLogo from "../public/images/index/spotify-logo.svg";
 
 const Footer: React.FC = (props) => {
   return (
     <footer className={styles.container}>
-      <article>
-        Music
+      <article className={styles.musicContainer}>
+        <p className={styles.musicHeader}>Recent vibes</p>
+
+        <span className={styles.musicTrackContainer}>
+          <Image src={SpotifyLogo} alt="The Spotify logo" />
+          <p className={styles.musicTrackTitle}>
+            Planes - Agorazein, C. Tangana
+          </p>
+        </span>
       </article>
-      <span>&copy; 2021</span>
+
       <article>
-        Weather
+        <span>&copy; 2021</span>
+      </article>
+
+      <article>
+        <span>xxx</span>
       </article>
     </footer>
   );
