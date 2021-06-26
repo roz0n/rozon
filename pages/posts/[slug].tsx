ff2d3465fe4e7a0bde91205017cadf44969840d7
Arnold Rozon <arnold@rozon.org>
2021-06-26 13:38:56 -0700

    Fixed useEffect bug

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -54,78 +54,80 @@ const Post: React.FC<{ post: Post }> = (props) => {
   const router = useRouter();
   const { post } = props;
 
+  useEffect(() => {
+    hljs.initHighlighting();
+  }, [post]);
+
   if (router.isFallback) {
     return <h1>Now loading...</h1>;
   } else {
-    useEffect(() => {
-      hljs.initHighlighting();
-      console.log("Called me");
-    }, []);
-  }
+    return (
+      <>
+        {/* // TODO: This should reuse Layout and make it dynamic */}
+        <header className={styles.header}>
+          <Image
+            src={ArrowBackIcon}
+            alt="An icon of an backward facing arrow"
+          />
+          <Link href="/">
+            <a>All posts</a>
+          </Link>
+        </header>
+        <div className={styles.layout__REMOVE}>
+          <div className={styles.categoryContainer}>
+            <p>Development</p>
+          </div>
 
-  return (
-    <>
-      {/* // TODO: This should reuse Layout and make it dynamic */}
-      <header className={styles.header}>
-        <Image src={ArrowBackIcon} alt="An icon of an backward facing arrow" />
-        <Link href="/">
-          <a>All posts</a>
-        </Link>
-      </header>
-      <div className={styles.layout__REMOVE}>
-        <div className={styles.categoryContainer}>
-          <p>Development</p>
+          <article className={styles.titleContainer}>
+            <h1 className={styles.title}>
+              Classifying Experimental Graphic Design Trends with Vision,
+              CoreML, AVKit, and JSDOM
+            </h1>
+
+            <section className={styles.postInfoContainer}>
+              <div className={styles.byLineContainer}>
+                <p className={styles.byLine}>By Arnold Rozon</p>
+                <p className={styles.byLine}>July 26, 1993</p>
+              </div>
+
+              <div className={styles.postControlsContainer}>
+                <span className={styles.commentsCountContainer}>
+                  <Image
+                    src={CommentsIcon}
+                    alt="A speech bubble icon to indicate comments"
+                  />
+                  <small className={styles.commentsCountLabel}>6</small>
+                </span>
+
+                <span className={styles.shareButtonContainer}>
+                  <Image src={ShareIcon} alt="A typical share icon" />
+                </span>
+              </div>
+            </section>
+
+            <section>
+              <h2 className={styles.subtitle}>
+                Aliquam hendrerit maximus congue. Quisque convallis, felis ac
+                iaculis porta, ante ligula euismod sapien, et molestie arcu eros
+                at lacus. Nunc eget orci convallis felis fringilla gravida,
+                consequat pretium mi. Quisque convallis, felis ac iaculis porta,
+                ante ligula.
+              </h2>
+            </section>
+
+            <figure className={styles.postDisplayImageContainer}>
+              {/* <Image alt="This blog post's display image" /> */}
+            </figure>
+          </article>
+
+          <section
+            className={styles.postBody}
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          ></section>
         </div>
-
-        <article className={styles.titleContainer}>
-          <h1 className={styles.title}>
-            Classifying Experimental Graphic Design Trends with Vision, CoreML,
-            AVKit, and JSDOM
-          </h1>
-
-          <section className={styles.postInfoContainer}>
-            <div className={styles.byLineContainer}>
-              <p className={styles.byLine}>By Arnold Rozon</p>
-              <p className={styles.byLine}>July 26, 1993</p>
-            </div>
-
-            <div className={styles.postControlsContainer}>
-              <span className={styles.commentsCountContainer}>
-                <Image
-                  src={CommentsIcon}
-                  alt="A speech bubble icon to indicate comments"
-                />
-                <small className={styles.commentsCountLabel}>6</small>
-              </span>
-
-              <span className={styles.shareButtonContainer}>
-                <Image src={ShareIcon} alt="A typical share icon" />
-              </span>
-            </div>
-          </section>
-
-          <section>
-            <h2 className={styles.subtitle}>
-              Aliquam hendrerit maximus congue. Quisque convallis, felis ac
-              iaculis porta, ante ligula euismod sapien, et molestie arcu eros
-              at lacus. Nunc eget orci convallis felis fringilla gravida,
-              consequat pretium mi. Quisque convallis, felis ac iaculis porta,
-              ante ligula.
-            </h2>
-          </section>
-
-          <figure className={styles.postDisplayImageContainer}>
-            {/* <Image alt="This blog post's display image" /> */}
-          </figure>
-        </article>
-
-        <section
-          className={styles.postBody}
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        ></section>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default Post;
