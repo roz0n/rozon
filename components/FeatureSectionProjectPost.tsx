66d274685bb83831251a22a160ed78ec40c9938b
Arnold Rozon <arnold@rozon.org>
2021-07-06 16:20:13 -0500

    Project section icons and styling

--- components/FeatureSectionProjectPost.tsx
+++ components/FeatureSectionProjectPost.tsx
@@ -1,6 +1,8 @@
 import styles from "../styles/FeatureSection/FeatureSectionProjectPost.module.css";
+import Image from "next/image";
 import Link from "next/link";
 import { FeatureSectionProjectPostProps } from "..";
+import SampleAppIcon from "../public/images/icons/sample-app-icon.svg";
 
 const FeatureSectionProjectPost: React.FC<FeatureSectionProjectPostProps> = (
   props
@@ -9,12 +11,37 @@ const FeatureSectionProjectPost: React.FC<FeatureSectionProjectPostProps> = (
 
   return (
     <article className={styles.container}>
-      <Link href={"/posts/[slug]"} as={`/posts/${slug}`}>
-        <a>
-          <h3 className={styles.title}>{title}</h3>
-        </a>
-      </Link>
-      <p className={styles.excerpt}>{excerpt}</p>
+      <section>
+        <article className={styles.headerContainer}>
+          <span className={styles.appIconContainer}>
+            <Image
+              src={SampleAppIcon}
+              height="65"
+              width="65"
+              alt={`${title} app icon`}
+            />
+          </span>
+
+          <span className={styles.titleContainer}>
+            <Link href={"/posts/[slug]"} as={`/posts/${slug}`}>
+              <a>
+                <h3 className={styles.title}>{title}</h3>
+              </a>
+            </Link>
+            <ul className={styles.tagList}>
+              <li className={styles.tagChip}>Swift</li>
+              <li className={styles.tagChip}>CoreML</li>
+              <li className={styles.tagChip}>AVKit</li>
+            </ul>
+          </span>
+        </article>
+
+        <article></article>
+      </section>
+
+      <section>
+        <p className={styles.excerpt}>{excerpt}</p>
+      </section>
     </article>
   );
 };
