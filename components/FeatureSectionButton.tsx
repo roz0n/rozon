5e2645aae0e671c376f26fd76e3894f81a9dd08e
Arnold Rozon <arnold@rozon.org>
2021-07-06 22:23:33 -0500

    More touchups for consistency

--- components/FeatureSectionButton.tsx
+++ components/FeatureSectionButton.tsx
@@ -2,7 +2,7 @@ import { FeatureSectionButtonProps } from "../";
 import styles from "../styles/FeatureSection/FeatureSectionButton.module.css";
 
 const FeatureSectionButton: React.FC<FeatureSectionButtonProps> = ({
-  label = "View all",
+  label = "View All",
 }) => {
   return <button className={styles.button}>{label}&nbsp;&nbsp;&#8594;</button>;
 };
