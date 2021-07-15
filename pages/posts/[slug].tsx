330c5f2ee01f31b1b7466ffd167a28cf0fa0590e
Arnold Rozon <arnold@rozon.org>
2021-07-15 18:31:10 -0500

    Removed telegram comments

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -7,7 +7,6 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import CommentsIcon from "../../components/Icons/CommentsIcon";
 import { ThemeContext } from "../_app";
-import TelegramComments from "../../components/TelegramComments";
 
 const { GHOST_API_KEY, GHOST_SITE_URL } = process.env;
 hljs.registerLanguage("swift", swift);
@@ -108,19 +107,6 @@ const Post: React.FC<{ post: GhostPost }> = (props) => {
             className={styles.postBody}
             dangerouslySetInnerHTML={{ __html: post.html }}
           ></section>
-
-          <TelegramComments
-            customColor="fb9f9f"
-            customHeight={450}
-            isDark
-            pageId="about-page"
-            showColorfulNames
-            showDislikes
-            showIconOutlines
-            websiteKey="tPChdq_H"
-            containerClassName="awesome-comments"
-            wrapperClassName="awesome-comments__wrapper"
-          />
         </div>
       </>
     );
