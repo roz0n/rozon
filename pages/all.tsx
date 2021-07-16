0fdad6304c18f25307e6a375dc39e8e4519bc4e5
Arnold Rozon <arnold@rozon.org>
2021-07-15 23:21:27 -0500

    Working on all page toolbar

--- pages/all.tsx
+++ pages/all.tsx
@@ -1,7 +1,28 @@
+import styles from "../styles/Pages/All/All.module.css";
+import { SearchIcon } from "@heroicons/react/outline";
+
 const All: React.FC = (props) => {
   return (
     <article>
-      <section>All page</section>
+      <section className={styles.headerContainer}>
+        <header className={styles.header}>
+          <article>{/* Back button */}</article>
+
+          <article className={styles.searchContainer}>
+            <span className={styles.searchIconWrapper}>
+              <SearchIcon width={18} height={18} />
+            </span>
+            <input
+              className={styles.searchInputField}
+              placeholder={"Search for something"}
+            />
+          </article>
+
+          <article>{/* Something else? */}</article>
+        </header>
+      </section>
+
+      <section className={styles.bodyContainer}>All page body</section>
     </article>
   );
 };
