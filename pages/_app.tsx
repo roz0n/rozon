2b44d71dd8c97c40d0c432516b3edc90c566e5e1
Arnold Rozon <arnold@rozon.org>
2021-07-19 03:29:36 -0500

    Mobile meta tag and overflow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -86,7 +86,10 @@ function MyApp({ Component, pageProps }) {
     <MainLayout>
       <Head>
         <title>Arnold Rozon &middot; Engineering &amp; Design</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
       </Head>
       <Header
         currentDisplayMode={currentDisplayMode}
