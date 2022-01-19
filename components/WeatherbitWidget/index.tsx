702c13627f39f0828325583a011ca536ff4f7c80
Arnold Rozon <arnold@rozon.org>
2022-01-18 20:12:08 -0600

    Redeploying

--- components/WeatherbitWidget/index.tsx
+++ components/WeatherbitWidget/index.tsx
@@ -5,23 +5,19 @@ import convertWeatherbitIcon from "../../utils/convertWeatherbitIcon";
 const WeatherbitWidget: React.FC<WeatherbitWidgetProps> = ({ data, error }) => {
   return data?.length && !error ? (
     <article className={styles.container}>
-      <header className={styles.headerContainer}>
-        {/* <p className={styles.title}>Latest Feels</p> */}
-      </header>
+      <header className={styles.headerContainer}>{/* <p className={styles.title}>Latest Feels</p> */}</header>
 
       <section className={styles.tempContainer}>
         <article className={styles.tempWrapper}>
           <span className={styles.tempIconContainer}>
-            <i
-              className={`wi ${convertWeatherbitIcon(data[0].weather.code)}`}
-            />
+            <i className={`wi ${convertWeatherbitIcon(data[0].weather.code)}`} />
           </span>
           <p>&nbsp;{Math.round(+data[0].temp)}&deg;</p>
         </article>
 
         <article className={styles.cityContainer}>
           <p>
-            in {data[0].city_name}, {data[0].state_code}, {data[0].country_code}
+            {data[0].city_name}, {data[0].state_code}, {data[0].country_code}
           </p>
         </article>
       </section>
